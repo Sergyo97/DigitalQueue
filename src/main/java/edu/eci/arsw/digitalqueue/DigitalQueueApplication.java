f50d9aff5f025de5d2f5137c724753b4094d090d
Daniel Vela <de.vela.lopez@gmail.com>
2019-08-27 21:52:59 -0500

    Added project base structure

---
+++ src/main/java/edu/eci/arsw/digitalqueue/DigitalQueueApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.digitalqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/digitalqueue/DigitalQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.digitalqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
