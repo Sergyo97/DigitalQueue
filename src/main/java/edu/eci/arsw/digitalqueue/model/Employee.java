94df75271ed8d6fd713d9da8bae09fc7b7d7f577
Daniel Vela <de.vela.lopez@gmail.com>
2019-10-01 21:34:42 -0500

    Fixed Wilson errors

--- src/main/java/edu/eci/arsw/digitalqueue/model/Employee.java
+++ src/main/java/edu/eci/arsw/digitalqueue/model/Employee.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@SuppressWarnings("unusef")
+@SuppressWarnings("unused")
 @Entity
 public class Employee{
 
@@ -12,7 +12,30 @@ public class Employee{
     private @Id
     @GeneratedValue
     Long id;
+    private String name;
+    private String email;
 
-    
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
