49b3fa8aa6adc4e6ef398a425104a6f3892f3c4f
Sergyo97 <sergio.rz.pz@gmail.com>
2019-10-01 19:06:51 -0500

    Controller updated

--- src/main/java/edu/eci/arsw/digitalqueue/controller/QueueController.java
+++ src/main/java/edu/eci/arsw/digitalqueue/controller/QueueController.java
@@ -1,13 +1,17 @@
 package edu.eci.arsw.digitalqueue.controller;
 
-import edu.eci.arsw.digitalqueue.model.Queue;
-import edu.eci.arsw.digitalqueue.service.QueueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import edu.eci.arsw.digitalqueue.model.Queue;
+import edu.eci.arsw.digitalqueue.service.QueueService;
 
 @RestController
 public class QueueController {
@@ -29,13 +33,13 @@ public class QueueController {
         return new ResponseEntity<>(queueService.findById(id), HttpStatus.ACCEPTED);
     }
 
-    @GetMapping("/queues/{id}")
+    @GetMapping("/queues/{name}")
     public ResponseEntity<?> findByName(@PathVariable String name){
         queueService.findByName(name);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
-    @DeleteMapping("/queues/{id}")
+    @DeleteMapping("/queues/{name}")
     public ResponseEntity<?> deleteByName(@PathVariable String name){
         queueService.deleteByName(name);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
