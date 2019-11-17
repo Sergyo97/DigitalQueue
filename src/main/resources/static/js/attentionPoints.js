47c2dbb9398f8a69ce8ecd447922ee9aa299f7ed
swilsonmelo <swilsonmelo@gmail.com>
2019-11-17 18:30:26 -0500

    modal attentionPoints con users y colas

--- src/main/resources/static/js/attentionPoints.js
+++ src/main/resources/static/js/attentionPoints.js
@@ -2,34 +2,37 @@
 var loading = document.getElementById('loading');
 var mensaje = document.getElementById('mensaje');
 var boton = document.getElementById('json_post');
-boton.addEventListener('click', function () {
-    loading.style.display = 'block';
-    //https://localhost:8080/users
 
-    axios.post('https://localhost:8080/attentionPoints', {
-        code: $('#recipient-code').val(),
-        queue: getQueueById(2)
+axios.get('https://localhost:8080/queues/')
+    .then(response => {        
+        var queues = response.data._embedded.queueList;
 
-    })
-        .then(function (res) {
-            if (res.status == 201) {
-                mensaje.innerHTML = 'El nuevo Post ha sido almacenado con id: ' + res.data.id;
-            }
-        })
-        .catch(function (err) {
-            console.log(err);
-        })
-});
+        queues.forEach(queue => {
+            $('#services').append(`<option>` + queue.name + `</option>`);
+        });
+        localStorage.setItem('queues', JSON.stringify(queues));
+    }
+    );
+
+
+axios.get('https://localhost:8080/users/')
+    .then(response => {
+        console.log(response)
+        var users = response.data._embedded.userList;
+
+        users.forEach(user => {
+            $('#users').append(`<option>` + user.email+ `</option>`);
+        });
+        localStorage.setItem('users', JSON.stringify(users));
+    }
+    );
 
 //http://localhost:8080/users
 axios.get('https://localhost:8080/attentionPoints')
     .then(response => {
         mydata = response.data;
         mydata = mydata._embedded.attentionPointList;
-        // $('#table1').bootstrapTable({
-        //     data: mydata
-        // });
-        console.log(mydata);
+        
         mydata.forEach(attentionPoint => {
             $('#attentionPointsTable').append(`
                 <tr>
@@ -49,38 +52,39 @@ axios.get('https://localhost:8080/attentionPoints')
     })
     .catch(e => {
         // Capturamos los errores
+    }
+    )
+
+boton.addEventListener('click', function () {
+    loading.style.display = 'block';
+    //https://localhost:8080/users
+
+    axios.post('https://localhost:8080/attentionPoints', {
+        code: $('#recipient-code').val(),
+        queue: JSON.parse(localStorage.getItem('queues')).find(queue => {
+            return queue.name == $('#services').val()
+        }),
+        employee: JSON.parse(localStorage.getItem('users')).find(user => {
+            return user.email == $('#services').val()
+        })
     })
+        .then(function (res) {
+            if (res.status == 201) {
+                mensaje.innerHTML = 'El nuevo Post ha sido almacenado con id: ' + res.data.id;
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+});
+
 
 function deleteAttentionPoint(id) {
     axios.delete("https://localhost:8080/attentionPoints/" + id).then(function (response) {
-
     })
 
 }
 
-var queue = getParameterByName('queueId'); // "lorem"
-
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
 
 
-function getQueueById(queueid) {
-    var queue
-    axios.get('https://localhost:8080/queues/' + queueid)
-        .then(response => {
-        queue = response.data
-        console.log(queue)
 
-        }).catch(e => {
-            // Capturamos los errores
-        })
-
-    return queue;
-}
