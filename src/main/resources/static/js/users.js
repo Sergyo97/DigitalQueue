20a6ec5ae42304a3d29469b3a2dacdd3ad27df69
swilsonmelo <swilsonmelo@gmail.com>
2019-11-19 22:45:56 -0500

    fixing code

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -4,8 +4,8 @@ var mensaje = document.getElementById('mensaje');
 var boton = document.getElementById('json_post');
 boton.addEventListener('click', () => {
     loading.style.display = 'block';
-    //https://digital-queue-404.herokuapp.com/users
-    axios.post('https://digital-queue-404.herokuapp.com/users', {
+    //http://digital-queue-404.herokuapp.com/users
+    axios.post('http://digital-queue-404.herokuapp.com/users', {
         name: $('#name').val(),
         email: $('#email').val(),
         password: $('#password').val(),
@@ -25,7 +25,7 @@ boton.addEventListener('click', () => {
 });
 
 //http://digital-queue-404.herokuapp.com/users
-axios.get('https://digital-queue-404.herokuapp.com/users')
+axios.get('http://digital-queue-404.herokuapp.com/users')
     .then(response => {
         mydata = response.data;
         mydata = mydata._embedded.userList;
