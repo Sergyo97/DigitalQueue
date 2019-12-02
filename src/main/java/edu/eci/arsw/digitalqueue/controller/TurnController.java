ca07eca8a1f0c787b1adc924c667bb8db8e3da4e
swilsonmelo <swilsonmelo@gmail.com>
2019-12-02 15:12:02 -0500

    pruebas atender turnos

--- src/main/java/edu/eci/arsw/digitalqueue/controller/TurnController.java
+++ src/main/java/edu/eci/arsw/digitalqueue/controller/TurnController.java
@@ -5,6 +5,7 @@ import edu.eci.arsw.digitalqueue.exception.NoTurnsInServiceException;
 import edu.eci.arsw.digitalqueue.exception.ServiceNotFoundException;
 import edu.eci.arsw.digitalqueue.exception.TurnAlreadyCancelledException;
 import edu.eci.arsw.digitalqueue.exception.TurnNotFoundException;
+import edu.eci.arsw.digitalqueue.model.Service;
 import edu.eci.arsw.digitalqueue.model.Turn;
 import edu.eci.arsw.digitalqueue.repository.ServiceRepository;
 import edu.eci.arsw.digitalqueue.repository.TurnRepository;
@@ -19,6 +20,8 @@ import java.net.URISyntaxException;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.stream.Collectors;
+
+
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
@@ -63,6 +66,37 @@ public class TurnController {
                 serviceRepository.findByName(service).orElseThrow(() -> new ServiceNotFoundException(service))
         ).size();
     }
+
+    @GetMapping("/countTurns")
+    public int countTurnsByQueue(@RequestParam String service) {
+        List<Turn> turns = turnRepository.findByService(
+            serviceRepository.findByName(
+                service).orElseThrow(() -> new ServiceNotFoundException(service)));
+        return turns.size();
+    }
+
+
+    @GetMapping("/totalWaiting")
+    public long countTotalTurnsWaiting() {
+        List<Service> services = serviceRepository.findAll();
+        long totalTurns = 0;
+        for(int i = 0; i < services.size(); i++){
+            Service service = services.get(i);
+            System.out.println(service.getName());
+            totalTurns += inQueue(service.getName());
+        }
+        System.out.println(totalTurns);
+        return totalTurns;
+    }
+
+    @GetMapping("/totalTickets")
+    public long countTotalTurns() {
+        List<Turn> turns = turnRepository.findAll();
+        long totalTurns = turns.size();
+        return totalTurns;
+    }
+
+
 
     @GetMapping("/next")
     public EntityModel<Turn> nextInQueue(@RequestParam String service) {
